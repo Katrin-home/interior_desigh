5b4fb931a696c16d89f526d96eaee372af7be93a
Ryzhkova Ekaterina <katrin.bomshtein@gmail.com>
2024-07-30 15:12:03 +0300

    upd body

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const url = 'https://interior-pt4c.onrender.com/api/emails';
 arrow.addEventListener('click', async (event) => {
     event.preventDefault();
     console.log(myInput.value)
-    // const formData = new FormData(myInput);
+    const formData = new FormData(myInput);
     const response = await fetch(url, {
         method: 'POST',
-        body: myInput.value
+        // body: JSON.stringify(myInput.value)
+        body: formData
     });
 
     if (response.ok) {
